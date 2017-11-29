2baccb49348ff5665a63db247e7cb46e16b508bd
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2017-11-29 17:48:22 +0100

    Add the application index, using the hexagon

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import PoetryReader from './domain/poetry-reader'
+import ConsoleAdapter from './infrastructure/console-adapter'
+
+// Instatiate the right-side adapter(s)
+
+// Instantiate the hexagon
+// (no right-side adapter -> hard coded poems here)
+const poetryReader = new PoetryReader()
+
+// Instantiate the left-side adapter(s)
+const consoleAdapter = new ConsoleAdapter(poetryReader)
+
+// App logic is only using left-side adapter(s).
+console.log('Here is some poetry:\n')
+consoleAdapter.ask()
